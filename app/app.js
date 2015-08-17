295001603adbf6a9c44157e0ebeed3a026b429f1
Andy Warren <andrew.warren@spectrakey.co.uk>
2015-08-17 16:07:44 +0100

    added navbar using angular-scroll

--- app/app.js
+++ app/app.js
@@ -9,6 +9,7 @@ angular.module("portfolio", [
     "ui.bootstrap",
     "ngSanitize",
     "ngMessages",
+    "duScroll",
     "portfolio.api",
     "portfolio.about",
     "portfolio.projects",
@@ -23,5 +24,25 @@ angular.module("portfolio", [
 .config([
     function () {
 
+    }
+])
+
+.run([
+    "$rootScope",
+    "$window",
+    function ($rootScope, $window) {
+        $rootScope.$on('duScrollspy:becameActive', function($event, $element){
+            //Automaticly update location
+            var hash = $element.prop('hash');
+            if (hash) {
+                history.replaceState(null, null, hash);
+            }
+            console.log($element);
+            //$rootScope.section = "home";
+            var section = hash.split('#')[1];
+            $rootScope.section = section;
+            $window.document.title = section + " | Andrew Warren"
+            $rootScope.$digest();
+        });
     }
 ]);
